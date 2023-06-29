169d9d5832ce3fe3fd623a18ad87d678230f70cf
suryacodess <helloosuryaprakash@gmail.com>
2023-06-29 14:11:25 +0530

    css solar system

---
+++ css solar system/script.js
@@ -0,0 +1,39 @@
+gsap.to(".star-1", {
+  x: -1000,
+  y: 1000,
+  duration: 6,
+  repeat: -1,
+  ease: "power",
+});
+gsap.to(".star-2", {
+  x: -1000,
+  y: 1000,
+  delay: 1,
+  duration: 6,
+  repeat: -1,
+  ease: "power",
+});
+gsap.to(".star-3", {
+  x: -1000,
+  y: 1000,
+  delay: 3,
+  duration: 6,
+  repeat: -1,
+  ease: "power",
+});
+gsap.to(".star-4", {
+  x: -1000,
+  y: 1000,
+  delay: 2,
+  duration: 6,
+  repeat: -1,
+  ease: "power",
+});
+gsap.to(".star-5", {
+  x: -1000,
+  y: 1000,
+  delay: 2,
+  duration: 6,
+  repeat: -1,
+  ease: "power",
+});
